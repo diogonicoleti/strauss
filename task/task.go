f6f42cc723260a9d000ea5bae178c79359f38ba7
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2024-07-06 14:04:56 -0300

    add docker run method

--- task/task.go
+++ task/task.go
@@ -1,9 +1,17 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"math"
+	"os"
 	"time"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 )
@@ -44,7 +52,7 @@ type Config struct {
 	AttachStdin   bool
 	AttachStdout  bool
 	AttachStderr  bool
-	ExposedPorts  nat.PortMap
+	ExposedPorts  nat.PortSet
 	Cmd           []string
 	Image         string
 	Cpu           float64
@@ -64,4 +72,63 @@ type DockerResult struct {
 	Action      string
 	ContainerId string
 	Result      string
+}
+
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(
+		ctx, d.Config.Image, image.PullOptions{})
+
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	io.Copy(os.Stdout, reader)
+
+	cc := container.Config{
+		Image:        d.Config.Image,
+		Tty:          false,
+		Env:          d.Config.Env,
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+
+	hc := container.HostConfig{
+		RestartPolicy: container.RestartPolicy{
+			Name: container.RestartPolicyMode(d.Config.RestartPolicy),
+		},
+		Resources: container.Resources{
+			Memory:   d.Config.Memory,
+			NanoCPUs: int64(d.Config.Cpu * math.Pow10(9)),
+		},
+		PublishAllPorts: true,
+	}
+
+	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf("Error creating container using image %s: %v\n",
+			d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerStart(ctx, resp.ID, container.StartOptions{})
+	if err != nil {
+		log.Printf("Error starting container %s: %v\n",
+			resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	// TODO: Add the runtime information
+	// d.Config.Runtime.ContainerID = resp.ID
+
+	out, err := d.Client.ContainerLogs(ctx, resp.ID,
+		container.LogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n",
+			resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	return DockerResult{ContainerId: resp.ID, Action: "start", Result: "success"}
 }
